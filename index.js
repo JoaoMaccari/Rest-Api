b63b9a961435d3fea5356a985cd74b7df711f070
João Elias Maccari <joaomaccari@hotmail.com>
2023-02-20 12:31:17 -0300

    att
    get id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
 
 app.get('/games', (req, res)=>{
+    res.status = 200;
     conexao.query('SELECT * FROM jogos', (error, results)=>{
         if(error){
             throw error;
@@ -13,6 +14,36 @@ app.get('/games', (req, res)=>{
             res.send(results)
         }
     })
+})
+
+app.get('/game/:id', (req, res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+
+        var id = parseInt(req.params.id)
+
+        conexao.query('SELECT * FROM jogos WHERE id =?', [id], (error, result) =>{
+            if(error){
+                
+                throw error;
+                
+            }else{
+                res.send(result)
+                
+            }
+        })
+
+        
+        // if(game != undefined){
+        //     res.json(game)
+        // }else{
+        //     res.sendStatus(404);
+        // }
+       
+
+       
+    }
 })
 
 app.listen(8000, () =>{
