cb0d138ea4ff573e5d3bdc1ee255d771172e003d
João Elias Maccari <joaomaccari@hotmail.com>
2023-02-21 11:29:13 -0300

    att
    consumo api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const conexao = require('./database/db')
+const conexao = require("./database/db")
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
 
